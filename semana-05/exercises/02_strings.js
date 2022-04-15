b0b3e3c26db9e33e63bc4d656f510bce30d4d300
Lucas Mattia <mattia.114.99@gmail.com>
2022-04-15 17:55:54 -0300

    BaSP-W05-2022: Exercise 2: strings

---
+++ semana-05/exercises/02_strings.js
@@ -0,0 +1,78 @@
+console.log('exercise 02: strings');
+
+//2. Strings
+//2.a-  Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar
+//      toUpperCase)
+
+var strbase = 'rosario central';
+console.log('original string: ' + strbase + '\nUpper case string: ' + strbase.toUpperCase());
+
+//2.b-  Crear una variable de tipo string con la menos 10 caracteres y generar un nuevo string con los últimos 3
+//      caracteres guardando el resultado en una nueva variable (utizar substring).
+
+//  Se utilizará la variable "strbase" declarada previamente
+
+var firsts_chars;
+
+firsts_chars = strbase.substring(0,5);
+
+console.log('Original string: ' + strbase + '\nfirst 5 characters: '+ firsts_chars);
+
+//2.c-  Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 3
+//       caracteres guardando el resultado en una nueva variable (utilizar substring).
+
+//  Se utilizará la variable "strbase" declarada previamente
+
+var lasts_chars;
+
+lasts_chars = strbase.substring(12,15);
+
+console.log('Original string: ' + strbase +'\nlast 3 characters: '+ lasts_chars);
+
+//2.d-  Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra
+//      en mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring,
+//      toUpperCase, toLowerCase y el operador +).
+
+//  Se utilizará la variable "strbase" declarada previamente
+
+var str_d;
+
+str_d = strbase.substring(0,1);
+str_d = str_d.toUpperCase();
+str_d = str_d + strbase.substring(1);
+
+console.log('Original string: ' + strbase +'\nFirst char upper case: '+ str_d);
+
+//2.e-  Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la
+//      posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf)
+
+//  Se utilizará la variable "strbase" declarada previamente
+
+var blankpos;
+
+blankpos = strbase.indexOf(' ');
+
+console.log('Original string: ' + strbase +'\nBlank space position: '+ blankpos);
+
+//2.f-  Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre
+//      medio). Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera
+//      letra de ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase,
+//      toLowerCase y el operador +).
+
+//  Se utilizarán variables declaradas previamente
+
+var str_f, str_f1, str_f2;
+
+str_f1 = strbase.substring(0,1);
+str_f1 = str_f1.toUpperCase();
+str_f1 = str_f1 + strbase.substring(1,blankpos);
+blankpos = strbase.indexOf(' ');
+str_f2 = strbase.substring(blankpos + 1, blankpos + 2);
+str_f2 = str_f2.toUpperCase();
+str_f2 = str_f2 + strbase.substring(blankpos + 2);
+str_f = str_f1 + ' ' + str_f2;
+
+console.log(str_f);
+
+console.log('Original string: ' + strbase +'\nOriginal string with first char of every word in upper case: '+ str_d);
+
